e77de2740566bb28b3c075be1181c66024f26683
fcomarcet1 <fcomarcet1@gmail.com>
2022-08-16 12:06:50 +0200

    Add Authentication middleware

--- app/Http/Middleware/Authentication.php
+++ app/Http/Middleware/Authentication.php
@@ -3,19 +3,24 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class Authentication
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @return Response|RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
-    {
+    public function handle(Request $request, Closure $next): Response|RedirectResponse {
+        // if not exists session user redirect to login page
+        if (!$request->session()->has('user')) {
+            return redirect()->route('login');
+        }
         return $next($request);
     }
 }
