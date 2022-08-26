2b94605b8ec9738b13bfba9a4b67617b8cfdc5a1
fcomarcet1 <fcomarcet1@gmail.com>
2022-08-26 13:29:36 +0200

    Add create role and user types migrations

---
+++ database/migrations/2022_08_26_105817_create_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('role', 150);
+            //$table->unsignedSmallInteger('level')->default(0);
+            $table->timestamps();
+            $table->softDeletes(); // this will create deleted_at field for softdelete
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('roles');
+    }
+};
